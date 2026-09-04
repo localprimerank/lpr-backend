771b47648365353a2f1c660d789d7b87684cb42e
luqman <localprimerank@gmail.com>
2026-09-04 15:14:19 +0500

    Update services.js

--- routes/services.js
+++ routes/services.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
 const { protect, admin } = require("../middleware/auth");
 const {
   getServices,
   getService,
+  getServiceBySlug,
   createService,
   updateService,
   deleteService,
 } = require("../controllers/serviceController");
 
 router.get("/", getServices);
+
+// NEW: must come BEFORE "/:id" so "/slug/xyz" isn't swallowed by the :id route
+router.get("/slug/:slug", getServiceBySlug);
+
 router.get("/:id", getService);
 router.post("/", protect, admin, createService);
 router.put("/:id", protect, admin, updateService);
